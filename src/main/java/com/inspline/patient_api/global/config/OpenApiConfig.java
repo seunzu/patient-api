9597046d6e4989b6449ba55fd259fc7096cbba58
seunzu <0123suh@gmail.com>
2026-04-24 02:35:44 +0900

    feat: Swagger UI 설정

---
+++ src/main/java/com/inspline/patient_api/global/config/OpenApiConfig.java
@@ -0,0 +1,30 @@
+package com.inspline.patient_api.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("InSpline Patient API")
+                        .version("1.0.0")
+                        .description("BE-2: Stateless API + Graceful Lifecycle + IaC 배포"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .description("JWT 토큰을 입력하세요")))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
+    }
+}
